cf58419c700774164ca5633c40b22d3eed826274
rinniss86 <rinniss86@gmail.com>
2012-05-17 23:47:34 -0400

    Done for the night

--- AppA/ASDCouch/app/_attachments/data.js
+++ AppA/ASDCouch/app/_attachments/data.js
@@ -3,31 +3,33 @@ $('#construction').live('pageinit', function(){
         console.log("button clicked");
         //******************************************** load json
         $.ajax({
-            url:'xhr/data.json', //this is where my json is located
+            url:'_view/orders', //this is where my json is located
             type:'GET', //What do we want to do?  get or post
             dataType:'json', //what type of data?  this one is json
             success :function(resp) {                     //if we find the file properly- do this
                 console.log("This is my JSON: ", resp);
-                for (var i = 0, len = resp.request.length; i < len; i++) {
-                    var item = resp.request[i];
-                    console.log('Item is: ', item);
-                    $('#json').after(' ' +
-                        '<p>' + item.pName[1] +
-                        ', ' + item.email[1] +
-                        ', ' + item.age[1] +
-                        ', ' + item.crave[1] +
-                        ', ' + item.fav[1] +
-                        ', ' + item.hunger[1] +
-                        ', ' + item.date[1] +
-                        ', ' + item.select[1] +
-                        ', ' + item.comment[1] +
-                        '</p>');
-                }
+                $.each(resp.rows, function(item, order))
+                    var name= orders.value.name
+                    var email= orders.value.email
+                    var age = orders.value.age
+                    var crave = orders.value.crave
+                    var fav = orders.value.fav
+                    var hunger = orders.value.hunger
+                    var date = orders.value.date
+                    var select = orders.value.select
+                    var comment = orders.value.comment
+                    $("#json").append(
+                        $('<li>').append(
+                            $('<a>').attr("href", "#").text(name)
+                        )
+                    )
+
             }
+            $("#json").listview('refresh');
         });
 
     });
-      // XML
+      /* XML
     var parseXML = function (xml){
         console.log(xml);
         $(xml).find("item").each(function(){
@@ -104,6 +106,6 @@ $('#construction').live('pageinit', function(){
             }
 
         })
-    })
+    }) */
 
 });
