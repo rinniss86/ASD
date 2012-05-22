987a4ec1de88d9d322ea00eb17e30344ed6e55ed
rinniss86 <rinniss86@gmail.com>
2012-05-22 08:08:01 -0400

    Morning commit. try to edit at work

--- AppA/ASDCouch/app/_attachments/main.js
+++ AppA/ASDCouch/app/_attachments/main.js
@@ -1,12 +1,12 @@
 $('#order').live('pageinit', function () {
 
     //getElementById Function
-   /* function $(x) {
-        var theElement = document.getElementById(x);
-        return theElement;
-    }
+    /* function $(x) {
+     var theElement = document.getElementById(x);
+     return theElement;
+     }
 
-    ; */
+     ; */
 
     //Create select field element and populate with options.
 
@@ -28,43 +28,44 @@ $('#order').live('pageinit', function () {
 
 
     /*function toggleControls(n) {
-        switch (n) {
-            case "on":
-                $('#order').style.display = "none";
-                $('#clear').style.display = "inline";
-                $('#displayLink').style.display = "none";
-                $('#addNew').style.display = "inline";
-                break;
-            case "off":
-                $('#order').style.display = "block";
-                $('#clear').style.display = "inline";
-                $('#displayLink').style.display = "inline";
-                $('#addNew').style.display = "none";
-                $('#items').style.display = "none";
-                break;
-            default:
-                return false;
-        }
-    } */
+     switch (n) {
+     case "on":
+     $('#order').style.display = "none";
+     $('#clear').style.display = "inline";
+     $('#displayLink').style.display = "none";
+     $('#addNew').style.display = "inline";
+     break;
+     case "off":
+     $('#order').style.display = "block";
+     $('#clear').style.display = "inline";
+     $('#displayLink').style.display = "inline";
+     $('#addNew').style.display = "none";
+     $('#items').style.display = "none";
+     break;
+     default:
+     return false;
+     }
+     } */
+
 
     function storeData(key) {
 
-       /* function getSelectedRadio() {
-            var radios = document.forms[0].food;
-            for (var i = 0; i < radios.length; i++) {
-                if (radios[i].checked) {
-                    craveValue = radios[i].value;
-                }
-            }
-        }  */
+        /* function getSelectedRadio() {
+         var radios = document.forms[0].food;
+         for (var i = 0; i < radios.length; i++) {
+         if (radios[i].checked) {
+         craveValue = radios[i].value;
+         }
+         }
+         }  */
 
-      /*  function getCheckBoxValue() {
-            if ($('#fav').checked) {
-                favValue = s('fav').value;
-            } else {
-                favValue = "No";
-            }
-        }   */
+        /*  function getCheckBoxValue() {
+         if ($('#fav').checked) {
+         favValue = s('fav').value;
+         } else {
+         favValue = "No";
+         }
+         }   */
 
         //If there is no key, this means this is a brand new item and we need a new key.
         if (!key) {
@@ -96,14 +97,18 @@ $('#order').live('pageinit', function () {
 
 
         //save to local storage: use stringify to convert
-        localStorage.setItem(id, JSON.stringify(item));
-        alert("Form Submitted");
+        $('#submit').on("click", function () {
+            //localStorage.setItem(id, JSON.stringify(item));
+            console.log("Form Submitted");
+        });
+
     }
 
     ;
 
+
     function getData() {
-        toggleControls("on");
+        //toggleControls("on");
         if (localStorage.length === 0) {
             alert("There is no data in Local Storage so default data was added.");
             autoFillData();
@@ -202,34 +207,34 @@ $('#order').live('pageinit', function () {
         //$('pword').value = item.pword[1];
         $('#email').value = item.email[1];
         $('#age').value = item.age[1];
-       // var radios = document.forms[0].food;
-       /* for (var i = 0; i < radios.length; i++) {
-            if (radios[i].value == "chinese" && item.crave[1] == "chinese") {
-                radios[i].setAttribute("checked", "checked");
-            } else if (radios[i].value == "spanish" && item.crave[1] == "spanish") {
-                radios[i].setAttribute("checked", "checked");
-            }
+        // var radios = document.forms[0].food;
+        /* for (var i = 0; i < radios.length; i++) {
+         if (radios[i].value == "chinese" && item.crave[1] == "chinese") {
+         radios[i].setAttribute("checked", "checked");
+         } else if (radios[i].value == "spanish" && item.crave[1] == "spanish") {
+         radios[i].setAttribute("checked", "checked");
+         }
 
-            if (radios[i].value == "indian" && item.crave[1] == "indian") {
-                radios[i].setAttribute("checked", "checked");
-            } else if (radios[i].value == "american" && item.crave[1] == "american") {
-                radios[i].setAttribute("checked", "checked");
-            }
-            if (radios[i].value == "italian" && item.crave[1] == "italian") {
-                radios[i].setAttribute("checked", "checked");
-            } else if (radios[i].value == "other" && item.crave[1] == "other") {
-                radios[i].setAttribute("checked", "checked");
-            }
+         if (radios[i].value == "indian" && item.crave[1] == "indian") {
+         radios[i].setAttribute("checked", "checked");
+         } else if (radios[i].value == "american" && item.crave[1] == "american") {
+         radios[i].setAttribute("checked", "checked");
+         }
+         if (radios[i].value == "italian" && item.crave[1] == "italian") {
+         radios[i].setAttribute("checked", "checked");
+         } else if (radios[i].value == "other" && item.crave[1] == "other") {
+         radios[i].setAttribute("checked", "checked");
+         }
 
-            if (item.fav[1] == "Yes") {
-                $('#fav').setAttribute("checked", "checked");
-            }
+         if (item.fav[1] == "Yes") {
+         $('#fav').setAttribute("checked", "checked");
+         }
 
 
-            if (item.fav[1] == "Yes") {
-                $('#fav').setAttribute("checked", "checked");
-            }
-        }    */
+         if (item.fav[1] == "Yes") {
+         $('#fav').setAttribute("checked", "checked");
+         }
+         }    */
 
         $('#date').value = item.date[1];
         $('#select').value = item.select[1];
@@ -238,148 +243,148 @@ $('#order').live('pageinit', function () {
         //Remove the initial listener from the input 'save lunch ' button
         //save.removeEventListener("click", storeData);
         //change Submit Button Value to Edit Button
-        $('#submit').on("click", function()
 
 
-       // $('#submit').value = "Edit Order";
+        // $('#submit').value = "Edit Order";
         //var editSubmit = $('#submit');
         //save the key value established in this function as a property of the editSubmit event
         //so we can use that value when we save the data we edited.
-       // editSubmit.addEventListener("click", validate);
+        // editSubmit.addEventListener("click", validate);
         //editSubmit.key = this.key;
-    );
-
-    function deleteItem() {
-        var ask = confirm("Are you sure you want to delete this order?");
-        if (ask) {
-            localStorage.removeItem(this.key);
-            alert("Order was deleted.");
-            window.location.reload();
-        } else {
-            alert("Order was NOT deleted");
-        }
-    }
-
-    function clearLocal() {
-        if (localStorage.length === 0) {
-            alert("There is no data to clear.");
-
-        } else {
-            localStorage.clear();
-            alert("Goodbye Hunger");
-            window.location.reload();
-            return false;
-        }
-    }
 
 
-    var parseOrderForm = function (data) {
-        // uses form data here;
-        console.log(data);
-    };
-
-    $(document).ready(function () {
-
-        var oform = $('#form');
-
-
-        oform.validate({
-            invalidHandler:function (form, validator) {
-
-            },
-            submitHandler:function () {
-                var data = oform.serializeArray();
-                parseOrderForm(data);
+        function deleteItem() {
+            var ask = confirm("Are you sure you want to delete this order?");
+            if (ask) {
+                localStorage.removeItem(this.key);
+                alert("Order was deleted.");
+                window.location.reload();
+            } else {
+                alert("Order was NOT deleted");
             }
+        }
+
+        function clearLocal() {
+            if (localStorage.length === 0) {
+                alert("There is no data to clear.");
+
+            } else {
+                localStorage.clear();
+                alert("Goodbye Hunger");
+                window.location.reload();
+                return false;
+            }
+        }
+
+
+        var parseOrderForm = function (data) {
+            // uses form data here;
+            console.log(data);
+        };
+
+        $(document).ready(function () {
+
+            var oform = $('#form');
+
+
+            oform.validate({
+                invalidHandler:function (form, validator) {
+
+                },
+                submitHandler:function () {
+                    var data = oform.serializeArray();
+                    parseOrderForm(data);
+                }
+            });
+
+
         });
 
 
-    });
+        /*function validate(e){
+         //define the elements we want to check
+         var getName = s('name');
+         var getEmail = s('email');
+         var getDate = s('date');
+         var getGroup = s('group');
+
+         //Reset Error Messages
+         errMsg.innerHTML = "";
+         getGroup.style.border = "1 px solid black";
+         getName.style.border = "1 px solid black";
+         getEmail.style.border = "1 px solid black";
+         getDate.style.border = "1 px solid black";
+         //Get Error Message
+         var messageAry = [];
+         //Group Validation
+         if(getGroup.value ==="--Where to Eat--"){
+         var groupError = "Choose where to eat.";
+         getGroup.style.border = "1px solid red";
+         messageAry.push(groupError);
+         }
+         //Name Validation
+         if(getName.value ===""){
+         var nameError = "Please enter a name."
+         getName.style.border = "1 px solid red";
+         messageAry.push(nameError);
+         }
+
+         //Email Validation
+         var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+         if(!(re.exec(getEmail.value))){
+         var emailError = "Please enter a valid email address.";
+         getEmail.style.border = "1px solid red";
+         messageAry.push(emailError);
+         }
+         //Date Validation
+         if(getDate.value ==="date"){
+         var dateError = "Enter a date";
+         getDate.style.border = "1 px solid red";
+         messageAry.push(dateError);
+         }
+
+         //If there were errors, display them on the screen
+         if(messageAry.length >= 1){
+         for(var i = 0, j = messageAry.length; i < j; i++){
+         var txt = document.createElement('li');
+         txt.innerHTML = messageAry[i];
+         errMsg.appendChild(txt);
+         }
+         e.preventDefault();
+         return false;
+         }else{
+         //If all is OK, save our data! Send the key value (which came from the editData Function).
+         // Remember this key value was passed through the editSubmit event listener as a property.
+         storeData(this.key);
+         }
+
+         }
+
+         //Slider Value Displayed
+         function hungerLevel(){
+         hVal = s('hungry').value;
+         console.log(hVal);
+         var makeDiv = document.getElementById("hungerNum");
+         makeDiv.innerHTML = hVal;
+         }
+         */
 
 
-    /*function validate(e){
-     //define the elements we want to check
-     var getName = s('name');
-     var getEmail = s('email');
-     var getDate = s('date');
-     var getGroup = s('group');
+        //Variable defaults
+        var whereToEat = ["--Where to Eat--", "Sit Down", "Pick Up", "Delivery", "Cook Your Own"],
+            craveVaule,
+            faveValue = "No",
+            errMsg = $('#errors');
+        //makeCats();
 
-     //Reset Error Messages
-     errMsg.innerHTML = "";
-     getGroup.style.border = "1 px solid black";
-     getName.style.border = "1 px solid black";
-     getEmail.style.border = "1 px solid black";
-     getDate.style.border = "1 px solid black";
-     //Get Error Message
-     var messageAry = [];
-     //Group Validation
-     if(getGroup.value ==="--Where to Eat--"){
-     var groupError = "Choose where to eat.";
-     getGroup.style.border = "1px solid red";
-     messageAry.push(groupError);
-     }
-     //Name Validation
-     if(getName.value ===""){
-     var nameError = "Please enter a name."
-     getName.style.border = "1 px solid red";
-     messageAry.push(nameError);
-     }
-
-     //Email Validation
-     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-     if(!(re.exec(getEmail.value))){
-     var emailError = "Please enter a valid email address.";
-     getEmail.style.border = "1px solid red";
-     messageAry.push(emailError);
-     }
-     //Date Validation
-     if(getDate.value ==="date"){
-     var dateError = "Enter a date";
-     getDate.style.border = "1 px solid red";
-     messageAry.push(dateError);
-     }
-
-     //If there were errors, display them on the screen
-     if(messageAry.length >= 1){
-     for(var i = 0, j = messageAry.length; i < j; i++){
-     var txt = document.createElement('li');
-     txt.innerHTML = messageAry[i];
-     errMsg.appendChild(txt);
-     }
-     e.preventDefault();
-     return false;
-     }else{
-     //If all is OK, save our data! Send the key value (which came from the editData Function).
-     // Remember this key value was passed through the editSubmit event listener as a property.
-     storeData(this.key);
-     }
-
-     }
-
-     //Slider Value Displayed
-     function hungerLevel(){
-     hVal = s('hungry').value;
-     console.log(hVal);
-     var makeDiv = document.getElementById("hungerNum");
-     makeDiv.innerHTML = hVal;
-     }
-     */
-
-
-    //Variable defaults
-    var whereToEat = ["--Where to Eat--", "Sit Down", "Pick Up", "Delivery", "Cook Your Own"],
-        craveVaule,
-        faveValue = "No",
-        errMsg = $('#errors');
-    //makeCats();
-
-    //Set Links & Submit Click Events
-   // var displayLink = $('#displayLink');
-    //displayLink.addEventListener("click", getData);
-   // var clearLink = $('#clear');
-   // clearLink.addEventListener("click", clearLocal);
-    var save = $('#submit');
-    //save.addEventListener("click", validate);
-    var hungerNum = $('#hungry');
-    //hungerNum.addEventListener("change", hungerLevel);
-});
+        //Set Links & Submit Click Events
+        // var displayLink = $('#displayLink');
+        //displayLink.addEventListener("click", getData);
+        // var clearLink = $('#clear');
+        // clearLink.addEventListener("click", clearLocal);
+        var save = $('#submit');
+        //save.addEventListener("click", validate);
+        var hungerNum = $('#hungry');
+        //hungerNum.addEventListener("change", hungerLevel);
+    }
+})
